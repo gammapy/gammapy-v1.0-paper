0466a128625c731271bbc1104e311d1d2b1f84c0
Axel Donath <axel.donath@mpi-hd.mpg.de>
2022-01-26 11:54:48 -0500

    Add minted rule

--- Snakefile
+++ Snakefile
@@ -1,6 +1,6 @@
 # User config
 configfile: "showyourwork.yml"
-    
+
 
 # Import the showyourwork module
 module showyourwork:
@@ -12,3 +12,14 @@ module showyourwork:
 
 # Use all default rules
 use rule * from showyourwork
+
+rule minted:
+    input:
+        "src/snippets/minted.py"
+    output:
+        "src/snippets/generated/gp_data.tex"
+    conda:
+        "environment.yml"
+    shell:
+        "cd src/snippets && python minted.py"
+
---
+++ src/snippets/minted.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import subprocess
+
+PATH = Path(__file__).parent
+
+SNIPPETS = PATH / "input"
+GENERATED = PATH / "generated"
+
+# for example 'frame=single' to frame the code
+OPTIONS = 'frame=single,style=friendly,linenos=1'
+
+if __name__ == '__main__':
+    GENERATED.mkdir(exist_ok=True)
+
+    filenames = SNIPPETS.glob("*.py")
+    for filename in filenames:
+        command = ["pygmentize"]
+        command += ["-O", f"verboptions={OPTIONS}"]
+        command += ["-f", "tex"]
+
+        filename_out = GENERATED / filename.name.replace(".py", ".tex")
+        command += ["-o", f"{filename_out}"]
+        command += [f"{filename}"]
+        subprocess.call(command)
