2415a5f8e35aec3e0e517991d3a17821ffc99b37
Axel Donath <axel.donath@mpi-hd.mpg.de>
2022-01-28 11:41:53 -0500

    Adapt authors script

--- scripts/authors.py
+++ scripts/authors.py
@@ -4,11 +4,15 @@ import subprocess
 import click
 from ruamel.yaml import YAML
 from pathlib import Path
+from astropy.table import Table, vstack
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 BASE_PATH = Path(__file__).parent.parent
+
+# TODO: get rid of the hard coded path, either download the file or
+#  introduce gammapy as sub-module to the paper repo
 PATH = Path("/Users/adonath/github/adonath/gammapy")
 CITATION_CFF = PATH / "CITATION.cff"
 
@@ -45,31 +49,51 @@ def get_citation_cff_authors_and_affiliations():
         affiliation = author_data.get("affiliation", "unknown")
         affiliations.append(affiliation)
 
-    return {
-        "authors": authors,
-        "affiliations": affiliations,
-    }
+    table = Table()
+    table["author"] = authors
+    table["affiliation"] = affiliations
+    table["label"] = "inst:unknown"
+
+    table_grouped = table.group_by("affiliation")
+
+    for idx, group in enumerate(table_grouped.groups):
+        affiliation = group[0]["affiliation"]
+
+        if affiliation == "unknown":
+            continue
+
+        mask = table["affiliation"] == affiliation
+        table["label"][mask] = f"inst:{idx}"
+
+    return table
 
 
 def write_author_file():
-    data = get_citation_cff_authors_and_affiliations()
+    table = get_citation_cff_authors_and_affiliations()
 
     author_str = "\\authorrunning{Deil, Donath, Terrier et al.}\n\n"
     author_str += "\\author{\n"
 
-    for idx, author in enumerate(data["authors"]):
-        label = f"inst:{idx}"
-        author_str += f"\t{author} \\inst{{\\ref{{{label}}}}} \\and\n"
+    for idx, row in enumerate(table):
+        author, label = row["author"], row["label"]
+        author_str += f"\t{author} \\inst{{\\ref{{{label}}}}}"
+        if idx < len(table) - 1:
+            author_str += " \\and\n"
 
-    author_str += "}\n\n"
+    author_str += "\n}\n\n"
 
     author_str += "\institute{\n"
-    for idx, affiliation in enumerate(data["affiliations"]):
-        label = f"inst:{idx}"
-        affiliation = affiliation.replace("&", "\&")
-        author_str += f"\t{affiliation} \\label{{{label}}} \\and\n"
 
-    author_str += "}\n"
+    table = table.group_by("affiliation")
+
+    for idx, group in enumerate(table.groups):
+        affiliation, label = group[0]["affiliation"], group[0]["label"]
+        affiliation = affiliation.replace("&", "\&")
+        author_str += f"\t{affiliation} \\label{{{label}}}"
+        if idx < len(table.groups) - 1:
+            author_str += " \\and\n"
+
+    author_str += "\n}\n"
 
     authors_file = BASE_PATH / "src/text/0-authors.tex"
 
