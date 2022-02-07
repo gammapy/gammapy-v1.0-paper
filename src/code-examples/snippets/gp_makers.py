2b473449eb061e38f60fc7dd9522a5a7f3b88b79
Terrier <rterrier@apc.in2p3.fr>
2022-02-07 23:37:00 +0100

    Modify code snippet

--- src/code-examples/snippets/gp_makers.py
+++ src/code-examples/snippets/gp_makers.py
@@ -1,4 +1,10 @@
-from gammapy.makers import MapDatasetMaker
+from gammapy.makers import MapDatasetMaker, FoVBackgroundMaker, SafeMaskMaker
 
 maker = MapDatasetMaker()
+bkg_maker = FoVBackgroundMaker()
+mask_maker = SafeMaskMaker()
+
 dataset = maker.run(dataset, observation)
+dataset = mask_maker.run(dataset,observation)
+dataset = bkg_maker.run(dataset,observation)
+
