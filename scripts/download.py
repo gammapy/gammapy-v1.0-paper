3059fe7f693fed203639f06fb0716092345c5dc8
Axel Donath <axel.donath@mpi-hd.mpg.de>
2022-02-14 15:20:46 -0500

    Adapt download script

--- scripts/download.py
+++ scripts/download.py
@@ -1,5 +1,5 @@
+import click
 from pathlib import Path
-from ruamel.yaml import YAML
 from gammapy.scripts.download import progress_download
 
 BASE_URL = "https://github.com/gammapy/gammapy-data/raw/master/"
@@ -7,25 +7,37 @@ PATH = Path(__file__).parent.parent
 PATH_DATA = PATH / "src/data"
 
 
+FILENAMES = [
+    "fermi-3fhl-gc-counts-cube.fits.gz",
+    "fermi-3fhl-gc-background-cube.fits.gz",
+    "fermi-3fhl-gc-exposure-cube.fits.gz",
+    "fermi-3fhl-gc-psf-cube.fits.gz",
+]
+
+
 def download_fermi_data():
     fermi_path = PATH_DATA / f"fermi-ts-map/input"
     fermi_path.mkdir(exist_ok=True, parents=True)
 
-    yaml = YAML()
-
-    config_file = PATH / "showyourwork.yml"
-    with config_file.open("r") as stream:
-        data = yaml.load(stream)
-
-    filenames = data["dependencies"]["src/figures/fermi_ts_map.py"]
-    filenames = [Path(_) for _ in filenames]
-
-    for filename in filenames:
-        destination = fermi_path / filename.name
-        source = BASE_URL + "fermi-3fhl-gc/" + filename.name
-        print(source)
+    for filename in FILENAMES:
+        destination = fermi_path / filename
+        source = BASE_URL + "fermi-3fhl-gc/" + filename
         progress_download(source, destination)
 
 
+DATASETS_REGISTRY = {
+    "fermi-gc": download_fermi_data,
+}
+
+
+@click.command()
+@click.argument(
+    "dataset", type=click.Choice(list(DATASETS_REGISTRY), case_sensitive=False)
+)
+def cli(dataset):
+    download_method = DATASETS_REGISTRY[dataset]
+    download_method()
+
+
 if __name__ == "__main__":
-    download_fermi_data()
+    cli()
