db2e82a4b2b9bd0ebb12e4659194fcafdd00427b
Laura Olivera <lauraoliverant@gmail.com>
2022-02-24 15:23:01 +0100

    fix typos and add to snippet

--- src/code-examples/snippets/gp_maps.py
+++ src/code-examples/snippets/gp_maps.py
@@ -1,16 +1,17 @@
-from gammapy.maps import Map
+from gammapy.maps import Map, MapAxis
 from astropy.coordinates import SkyCoord
-from astripy import units as u
+from astropy import units as u
 
 skydir = SkyCoord("0d", "5d", frame="galactic")
+energy_axis = MapAxis.from_bounds(lo_bnd=0.1, hi_bnd=100, nbin=5, unit='TeV', interp='log', name='energy')
 
 # Create a WCS Map
 m_wcs = Map.create(
     binsz=0.1,
     map_type="wcs",
     skydir=skydir,
-    width=[10.0, 8.0] * u.deg
-)
+    width=[10.0, 8.0] * u.deg,
+    axes = [energy_axis])
 
 
 # Create a HPX Map
@@ -18,5 +19,12 @@ m_hpx = Map.create(
     binsz=0.1,
     map_type="hpx",
     skydir=skydir,
-    width=10.0 * u.deg
-)
+    width=10.0 * u.deg,
+    axes = [energy_axis])
+
+# Create a region map
+region = "galactic;circle(0, 5, 1)"
+m_region = Map.create(
+    region = region,
+    map_type='region',
+    axes = [energy_axis])
