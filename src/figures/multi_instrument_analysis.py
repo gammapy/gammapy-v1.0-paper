04597bcf0c82b3aab66078e7b13ec1f372f9beea
cosimoNigro <cosimonigro2@gmail.com>
2022-03-07 17:46:08 +0100

    added script to generate dummy multi-instrument figure

--- src/figures/multi_instrument_analysis.py
+++ src/figures/multi_instrument_analysis.py
@@ -1,9 +1,11 @@
 import config
+import numpy as np
 import matplotlib.pyplot as plt
 
-figsize = config.FigureSizeAA(aspect_ratio=1.618, width_aa="one-column")
-fig, ax  = plt.subplots(figsize=figsize)
+figsize = config.FigureSizeAA(aspect_ratio=1.618, width_aa="single-column")
+fig, ax  = plt.subplots(1, 1, figsize=figsize.inch)
 
-
+x = np.ones(5)
+ax.plot(x, x, marker="o")
 
 fig.savefig("multi_instrument_analysis.pdf")
