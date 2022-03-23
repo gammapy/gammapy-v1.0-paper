b558fae15387e1828801efa4922c966ba33d2cce
Terrier <rterrier@apc.in2p3.fr>
2022-03-23 19:13:11 +0100

    cleaner snippet

--- src/code-examples/snippets/gp_stats.py
+++ src/code-examples/snippets/gp_stats.py
@@ -3,9 +3,12 @@ from gammapy.stats import WStatCountsStatistic
 n_on = [13, 5, 3]
 n_off = [11, 9, 20]
 alpha = [0.8, 0.5, 0.1]
-stat = WStatCountsStatistic(n_on=13, n_off=11, alpha=1. / 2)
-print(f"Excess  : {stat.n_sig:.2f}")
-print(f"sqrt(TS): {stat.sqrt_ts:.2f}")
-print(f"Errors (1 sigma): {stat.compute_errn(1.):.2f} -- {stat.compute_errp(1.):.2f}")
-print(f"Errors (2 sigma): {stat.compute_errn(2.):.2f} -- {stat.compute_errp(2.):.2f}")
+stat = WStatCountsStatistic(n_on, n_off, alpha)
+# Excess
+stat.n_sig
+# Significance
+stat.sqrt_ts
+# Asymmetrical rrors
+stat.compute_errn(1.)
+stat.compute_errp(1.)
 
