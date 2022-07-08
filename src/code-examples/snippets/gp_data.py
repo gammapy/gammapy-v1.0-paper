ef232b9bf20c641e7c138c06247c26392abce7dc
Axel Donath <axel.donath@mpi-hd.mpg.de>
2022-07-07 23:57:22 -0400

    Further extend code examples

--- src/code-examples/snippets/gp_data.py
+++ src/code-examples/snippets/gp_data.py
@@ -1,5 +1,14 @@
 from gammapy.data import DataStore
 
-data_store = DataStore.from_dir("$GAMMAPY_DATA")
-obs_ids = [1, 2, 3]
+data_store = DataStore.from_dir(
+    "$GAMMAPY_DATA/hess-dl3-dr1"
+)
+
+obs_ids = [23523, 23526, 23559, 23592]
+
 observations = data_store.get_observations(obs_ids)
+
+for obs in observations:
+    print(f"Observation id: {obs.obs_id}")
+    print(f"Number of events: {len(obs.events)}")
+    print(f"Max. area: {obs.aeff.quantity.max()}")
