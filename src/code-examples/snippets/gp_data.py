7ad1629afcd843357770d76137c23a1d546558d9
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2022-11-23 12:19:42 +0100

    Update code examples

--- src/code-examples/snippets/gp_data.py
+++ src/code-examples/snippets/gp_data.py
@@ -1,13 +1,13 @@
 from gammapy.data import DataStore
 
 data_store = DataStore.from_dir(
-    based_dir="$GAMMAPY_DATA/hess-dl3-dr1"
+    base_dir="$GAMMAPY_DATA/hess-dl3-dr1"
 )
 
 obs_ids = [23523, 23526, 23559, 23592]
 
 observations = data_store.get_observations(
-    obs_ids=obs_id, skip_missing=True
+    obs_id=obs_ids, skip_missing=True
 )
 
 for obs in observations:
