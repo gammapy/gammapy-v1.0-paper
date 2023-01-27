5bfa2fabe0f82f561285bec5dda1f1bce347f0f5
Axel Donath <axel.donath@cfa.harvard.edu>
2023-01-27 18:15:55 -0500

    Add code example output

--- src/code-examples/minted.py
+++ src/code-examples/minted.py
@@ -1,6 +1,6 @@
-from pathlib import Path
-import subprocess
 import logging
+import subprocess
+from pathlib import Path
 
 log = logging.getLogger(__file__)
 logging.basicConfig(level=logging.DEBUG)
@@ -9,11 +9,19 @@ PATH = Path(__file__).parent
 
 SNIPPETS = PATH / "snippets"
 GENERATED = PATH / "generated"
+GENERATED_OUTPUT = PATH / "generated-output"
+
+EXCLUDE_OUTPUT = [
+    "gp_catalogs.py",
+    "gp_estimators.py",
+    "gp_makers.py",
+]
 
 # for example 'frame=single' to frame the code
-OPTIONS = 'frame=lines,style=friendly,linenos=0'
+OPTIONS = "frame=lines,style=friendly,linenos=0"
+OPTIONS_OUTPUT = "frame=lines,style=nord,linenos=0"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     GENERATED.mkdir(exist_ok=True)
 
     filenames = SNIPPETS.glob("*.py")
@@ -27,3 +35,30 @@ if __name__ == '__main__':
         command += [f"{filename}"]
         log.info(f"Executing {' '.join(command)}")
         subprocess.call(command)
+
+    filenames = SNIPPETS.glob("*.py")
+    GENERATED_OUTPUT.mkdir(exist_ok=True)
+
+    for filename in filenames:
+        if filename.name in EXCLUDE_OUTPUT:
+            continue
+
+        command = ["pygmentize"]
+        command += ["-O", f"verboptions={OPTIONS_OUTPUT}"]
+        command += ["-f", "tex"]
+
+        filename_out = GENERATED_OUTPUT / filename.name.replace(".py", ".tex")
+        command += ["-o", f"{filename_out}"]
+        log.info(f"Executing {' '.join(command)}")
+
+        p = subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        proc = subprocess.Popen(["python", str(filename)], stdout=subprocess.PIPE)
+        out = proc.communicate()[0]
+
+        stdout_data = p.communicate(input=out)[0]
