944fdb88057e71531ff6f586110d8a6705ae148f
Axel Donath <axel.donath@cfa.harvard.edu>
2023-01-27 17:25:01 -0500

    Adapt code examples

--- src/code-examples/snippets/gp_catalogs.py
+++ src/code-examples/snippets/gp_catalogs.py
@@ -1,10 +1,13 @@
+import matplotlib.pyplot as plt
+
 from gammapy.catalog import CATALOG_REGISTRY
 
 catalog = CATALOG_REGISTRY.get_cls("4fgl")()
 print("Number of sources :", len(catalog.table))
 
 source = catalog["PKS 2155-304"]
-model = source.sky_model()
-source.flux_points.plot()
 
-source.lightcurve().plot()
+_, axes = plt.subplots(ncols=2)
+source.flux_points.plot(ax=axes[0], sed_type="e2dnde")
+
+source.lightcurve().plot(ax=axes[1])
--- src/code-examples/snippets/gp_makers.py
+++ src/code-examples/snippets/gp_makers.py
@@ -1,27 +1,25 @@
 import astropy.units as u
-from gammapy.data import DataStore
-from gammapy.maps import MapAxis, WcsGeom
-from gammapy.datasets import MapDataset
-from gammapy.makers import (
-    MapDatasetMaker,
-    FoVBackgroundMaker,
-    SafeMaskMaker,
-)
 
-data_store = DataStore.from_dir(
-    base_dir="$GAMMAPY_DATA/hess-dl3-dr1"
-)
+from gammapy.data import DataStore
+from gammapy.datasets import MapDataset
+from gammapy.makers import FoVBackgroundMaker, MapDatasetMaker, SafeMaskMaker
+from gammapy.maps import MapAxis, WcsGeom
+
+data_store = DataStore.from_dir(base_dir="$GAMMAPY_DATA/hess-dl3-dr1")
 
 obs = data_store.obs(23523)
 
 energy_axis = MapAxis.from_energy_bounds(
-    energy_min="1 TeV", energy_max="10 TeV", nbin=6,
+    energy_min="1 TeV",
+    energy_max="10 TeV",
+    nbin=6,
 )
 
 geom = WcsGeom.create(
     skydir=(83.633, 22.014),
-    width=3 * u.deg,
+    width=(4, 3) * u.deg,
     axes=[energy_axis],
+    binsz=0.02 * u.deg,
 )
 
 empty = MapDataset.create(geom=geom)
@@ -40,4 +38,4 @@ bkg_maker = FoVBackgroundMaker(
 dataset = maker.run(empty, observation=obs)
 dataset = bkg_maker.run(dataset, observation=obs)
 dataset = mask_maker.run(dataset, observation=obs)
-print(dataset)
+dataset.peek()
